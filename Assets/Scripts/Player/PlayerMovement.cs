07b85ecf4fc97d29ccd1291d42b08fd9daa2a128
DennysEnti <dennys.hernandez@enti.cat>
2023-10-28 19:27:03 +0200

    Jump and movement done
    when jump, you allways do the next jump and no reset in the first jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private Camera camera;
+    [Header("Camera")]
+    [SerializeField] private Camera camera;
+
 
     private CharacterController controller;
     private Input_Manager inputManager;
@@ -15,20 +17,29 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 finalVelocity = Vector3.zero;
     private Vector3 followDirecction = Vector3.zero;
 
-    [SerializeField] private float velocity = 0f; // velocityXZ
-    [SerializeField] private float maxVelocity = 5f; // velocityXZ
-    [SerializeField] private float acceleration = 5f; 
-    [SerializeField] private float decceleration = 5f; 
 
-    [SerializeField] private float gravity = 20f;
-    [SerializeField] private float jumpForce = 8f;
-    [SerializeField] private float coyoteTime = 1f;
-
-    [SerializeField] private int maxJumps = 2; // 3 jumps
-    [SerializeField] private float jumpIncrement = 5f;
-    [SerializeField] private int jumpCounter = 0;
     
+    [Header("Movement")]
+    [SerializeField] private float maxVelocity = 8f; // velocityXZ
+                     private float velocity = 0f; // velocityXZ
+    [SerializeField] private float acceleration = 5f; 
+    [SerializeField] private float decceleration = 5f;
+
+
+    [Header("Rotation")]
     [SerializeField] private float rotationSpeed = 1f;
+
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 8f;
+    private float gravity = 20f;
+
+    private int maxJumps = 2; // 3 jumps
+    private float jumpIncrement = 5f;
+    private int jumpCounter = 0;
+
+
+
 
     private void Awake()
     {
@@ -51,7 +62,6 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = inputManager.GetLeftAxisValue().x;
         float verticalInput = inputManager.GetLeftAxisValue().y;
 
-
         // Calcular dirección XZ (movimiento)
         Vector3 direction = Quaternion.Euler(0f, camera.transform.eulerAngles.y, 0f) * new Vector3(horizontalInput, 0f, verticalInput);
         direction.Normalize();
@@ -59,9 +69,9 @@ public class PlayerMovement : MonoBehaviour
         if (direction != Vector3.zero)
         {
             //rotation
-            Quaternion desiredRotation = Quaternion.LookRotation(direction, Vector3.up);
+            Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, 0.5f * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.5f * Time.deltaTime);
             gameObject.transform.forward = direction;
 
             // acceleration
@@ -70,6 +80,7 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
+            // decceleration
             velocity -= decceleration * Time.deltaTime;
             direction.x = followDirecction.x;
             direction.z = followDirecction.z;
@@ -85,117 +96,53 @@ public class PlayerMovement : MonoBehaviour
         direction.y = -1f;
 
         // Aplica gravedad dependiendo si esta en el suelo o no
-        if (controller.isGrounded)
-        {
-            finalVelocity.y = direction.y * gravity * Time.deltaTime;
-        }
-        else
-        {
-            finalVelocity.y += direction.y * gravity * Time.deltaTime;
-        }
-        Debug.Log(finalVelocity.y);
+        finalVelocity.y += direction.y * gravity * Time.deltaTime;
+
         controller.Move(finalVelocity * Time.deltaTime);
     }
+
 
     private void TripleJump()
     {
         // Calcular gravedad 
-        if (controller.isGrounded)
+        if (inputManager.GetJumpButtonPressed() && controller.isGrounded)
         {
-            if (inputManager.GetJumpButtonPressed())
+            finalVelocity.y = jumpForce + jumpIncrement * jumpCounter;
+
+            if (jumpCounter < maxJumps)
             {
-                Debug.Log("jumpiiiiing");
-
-                finalVelocity.y = jumpForce + jumpIncrement * jumpCounter;
-
-                if (jumpCounter < maxJumps)
-                {
-                    jumpCounter++;
-                }
-                else
-                {
-                    jumpCounter = 0;
-                }
+                jumpCounter++;
             }
-
-            // El contador va bajando hasta que llega a 0
-            //jumpsDelay -= Time.deltaTime;
-            /*
-            // Primer Salto
-            //if (Input_Manager._INPUT_MANAGER.GetJumpButtonPressed() && maxJumps == 3)
-            if (Input.GetKey(KeyCode.Space) && maxJumps == 3)
+            else
             {
-                finalVelocity.y = jumpForce;
-                maxJumps--;
-                jumpsDelay = 3f;
-
+                jumpCounter = 0;
             }
+        }
+    }
 
-            // Segundo Salto
-            else if (Input.GetKey(KeyCode.Space) && maxJumps == 2)
-            {
-                finalVelocity.y = jumpForce + 5;
-                maxJumps--;
-                jumpsDelay = 3f;
+    private void Crouch()
+    {
+        if (inputManager.GetCrouchButtonPressed())
+        {
+        }
 
-            }
-
-            // Tercer Salto.
-            else if (Input.GetKey(KeyCode.Space) && maxJumps == 1)
-            {
-                finalVelocity.y = jumpForce + 10;
-                maxJumps--;
-                jumpsDelay = 3f;
-
-            }
-
-            else if (maxJumps == 0 && jumpsDelay < 3f)
-            {
-                finalVelocity.y = direction.y * gravity * Time.deltaTime;
-                coyoteTime = 1f;
-                maxJumps = 3;
-                jumpsDelay = 3f;
-            }
-            */
-
-            /*
-            // Resetear el contador cuando llega a 0
-            if (jumpsDelay <= 0f || maxJumps == 0)
-            {
-                jumpsDelay = 3f;
-                maxJumps = 3; // Reiniciar el número de saltos disponibles
-            }
-
-            // Primer Salto
-            if (inputManager.GetJumpButtonPressed() && maxJumps > 0)
-            {
-                finalVelocity.y = jumpForce + (3 - maxJumps) * 5;
-                maxJumps--;
-                jumpsDelay = 3f;
-            }
-
-            else if (maxJumps == 0 && jumpsDelay < 3f)
-            {
-                finalVelocity.y = direction.y * gravity * Time.deltaTime;
-                coyoteTime = 1f;
-            }
+        if (Input.GetKey(KeyCode.LeftShift) && crouch == false)
+        {
+            crouch = true;
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift) && crouch == true)
+        {
+            crouch = false;
+        }
+        if (crouch == true)
+        {
+            finalVelocity *= 0.5f;
+            controller.height = 1f;
 
         }
         else
         {
-            finalVelocity.y += direction.y * gravity * Time.deltaTime;
-            coyoteTime -= Time.deltaTime;
-
-            if (inputManager.GetJumpButtonPressed() && coyoteTime >= 0f)
-            {
-                finalVelocity.y = jumpForce;
-                coyoteTime = 0f;
-            }*/
+            controller.height = 2f;
         }
-    }
-
-    private void Couch()
-    {
-
     }
 }
